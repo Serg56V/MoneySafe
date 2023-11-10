592678a34c086f40e42c0fa2579c8785cce68067
Siarhei VASILIUK <102259012+Serg56V@users.noreply.github.com>
2023-11-10 19:21:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+import { convertStringNumber } from './convertStringNumber.js';
+
+const financeForm = document.querySelector('.finance__form');
+const financeAmount = document.querySelector('.finance__amount');
+const financeReport = document.querySelector('.finance__report');
+const report = document.querySelector('.report');
+const reportClose = document.querySelector('.report__close');
+console.log(reportClose);
+
+let amount = 0;
+financeAmount.textContent = amount;
+
+financeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const typeOperation = event.submitter.dataset.typeOperation;
+  
+  const changeAmount = Math.abs(convertStringNumber(financeForm.amount.value));
+
+  if (typeOperation === 'income') {
+    amount += changeAmount;
+  }
+  
+  if (typeOperation === 'expenses') {
+    amount -= changeAmount;
+  }
+
+  financeAmount.textContent = `${amount.toLocaleString()} ₽`;
+});
+
+financeReport.addEventListener("click", () => {
+  report.classList.add('report__open');
+});
+
+reportClose.addEventListener("click", () => {
+  report.classList.remove('report__open');
+});
